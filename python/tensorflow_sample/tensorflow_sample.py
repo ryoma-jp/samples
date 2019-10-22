cad7ce1cc0a702550a9c775c0e8daaea0fe4814c
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2019-10-23 04:08:05 +0900

    三角関数のサンプルを追加

--- python/tensorflow_sample/tensorflow_sample.py
+++ python/tensorflow_sample/tensorflow_sample.py
@@ -56,6 +56,11 @@ class CalcMatrix():
         print('inv(np.matmul(mat1, mat2)) = {}'.format(self.inv))
         print('inv.shape = {}'.format(self.inv.shape))
 
+        # --- 三角関数 ---
+        print('sin(mat1) = {}'.format(self.sin))
+        print('cos(mat1) = {}'.format(self.cos))
+        print('tan(mat1) = {}'.format(self.tan))
+
         return
 
 class CalcMatrixNumpy(CalcMatrix):
@@ -68,6 +73,10 @@ class CalcMatrixNumpy(CalcMatrix):
         self.matmul = np.matmul(mat1, self.transpose)   # 内積2
         self.det = np.linalg.det(self.matmul)   # 行列式
         self.inv = np.linalg.inv(self.matmul)   # 逆行列
+        x = np.radians(mat1 * 360)
+        self.sin = np.sin(x)
+        self.cos = np.cos(x)
+        self.tan = np.tan(x)
         return
 
 class CalcMatrixTf(CalcMatrix):
@@ -83,20 +92,27 @@ class CalcMatrixTf(CalcMatrix):
         tf_matmul = tf.matmul(tf_mat1, tf_transpose)    # 内積2
         tf_det = tf.linalg.det(tf_matmul)       # 行列式
         tf_inv = tf.linalg.inv(tf_matmul)       # 逆行列
+#        x = tf.constant([30. * 0.017453292519943295, 45. * 0.017453292519943295, 60. * 0.017453292519943295, 90. * 0.017453292519943295])
+        x = self.tf_radians(mat1 * 360)
+        tf_sin = tf.compat.v1.math.sin(x)
+        tf_cos = tf.compat.v1.math.cos(x)
+        tf_tan = tf.compat.v1.math.tan(x)
 
         sess = tf.compat.v1.Session()
-        self.add, self.sub, self.mul, self.transpose, self.dot, self.matmul, self.det, self.inv = \
-                sess.run([tf_add, \
-                          tf_sub, \
-                          tf_mul, \
+        self.add, self.sub, self.mul, self.transpose, self.dot, self.matmul, self.det, self.inv, \
+        self.sin, self.cos, self.tan = \
+                sess.run([tf_add, tf_sub, tf_mul, \
                           tf_transpose, \
-                          tf_dot, \
-                          tf_matmul, \
-                          tf_det, \
-                          tf_inv], feed_dict={tf_mat1:mat1, tf_mat2:mat2})
+                          tf_dot, tf_matmul, \
+                          tf_det, tf_inv, \
+                          tf_sin, tf_cos, tf_tan], feed_dict={tf_mat1:mat1, tf_mat2:mat2})
         sess.close()
 
         return
+
+    def tf_radians(self, deg):
+        pi_on_180 = 0.017453292519943295
+        return tf.convert_to_tensor(deg * 0.017453292519943295)
 
 def main():
     # --- 引数処理 ---
@@ -127,6 +143,9 @@ def main():
     print('matmul : {}'.format(np.max(np.abs(calc_mat_np.matmul-calc_mat_tf.matmul))))
     print('det : {}'.format(np.max(np.abs(calc_mat_np.dot-calc_mat_tf.dot))))
     print('inv : {}'.format(np.max(np.abs(calc_mat_np.inv-calc_mat_tf.inv))))
+    print('sin : {}'.format(np.max(np.abs(calc_mat_np.sin-calc_mat_tf.sin))))
+    print('cos : {}'.format(np.max(np.abs(calc_mat_np.cos-calc_mat_tf.cos))))
+    print('tan : {}'.format(np.max(np.abs(calc_mat_np.tan-calc_mat_tf.tan))))
 
     return
 
