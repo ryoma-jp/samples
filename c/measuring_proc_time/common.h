78419547d2fe67e0c11e1e8561ca8f4a06575edd
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2019-10-31 04:07:10 +0900

    doxygen対応

--- c/measuring_proc_time/common.h
+++ c/measuring_proc_time/common.h
@@ -1,54 +1,48 @@
 
-#ifndef __COMMON_H__
-#define __COMMON_H__
+#ifndef __TEMPLATE_COMMON_H__
+#define __TEMPLATE_COMMON_H__
 
-/*******************************************************
- * インクルードファイル
- *******************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-
-/*******************************************************
- * 定数定義
- *******************************************************/
+#include <time.h>
 
 /**
- * 標準出力の表示レベル
- *   MY_PRINT_LVLに定義した情報を表示する
+ * @def MY_PRINT_LVL_INFO
+ * @brief 処理の進行情報などを表示する標準出力レベル
  */
-#define MY_PRINT_LVL_INFO	(0x00000001)	/**< 処理の進行情報など */
-#define MY_PRINT_LVL_WARN	(0x00000002)	/**< 警告に相当する情報 */
-#define MY_PRINT_LVL_ERROR	(0x00000004)	/**< 処理エラーに相当する情報 */
-#define MY_PRINT_LVL		(MY_PRINT_LVL_INFO | MY_PRINT_LVL_WARN | MY_PRINT_LVL_ERROR) /**< 表示レベル */
-
-/*******************************************************
- * 関数マクロ
- *******************************************************/
+#define MY_PRINT_LVL_INFO	(0x00000001)
 
 /**
- * デバッグ用 printf の定義
- * 出力有無を決めるレベルは MY_PRINT_LVL で定義する
+ * @def MY_PRINT_LVL_WARN
+ * @brief 警告に相当する情報を表示する標準出力レベル
+ */
+#define MY_PRINT_LVL_WARN	(0x00000002)
+
+/**
+ * @def MY_PRINT_LVL_ERROR
+ * @brief 処理エラーに相当する情報を表示する標準出力レベル
+ */
+#define MY_PRINT_LVL_ERROR	(0x00000004)	/**< 処理エラーに相当する情報 */
+
+/**
+ * @def MY_PRINT_LVL
+ * @brief 標準出力する内容を定義
+ * @details 標準出力レベルを"|"で繋いで標準出力する内容を設定する
+ */
+#define MY_PRINT_LVL		(MY_PRINT_LVL_INFO | MY_PRINT_LVL_WARN | MY_PRINT_LVL_ERROR)
+
+/**
+ * @def MY_PRINT
+ * @brief 標準出力するプログラム共通の関数マクロ
+ * @details MY_PRINT_LVLで定義された内容を標準出力する
  */
 #define MY_PRINT(level, ...) { \
 		if (level & MY_PRINT_LVL) { \
-			struct timeval _tv; \
-			gettimeofday(&_tv, NULL); \
-			printf("[LVL:%08x][%6d.%06d] %s(L.%d) : ", level, (int)_tv.tv_sec, (int)_tv.tv_usec, __func__, __LINE__); \
+			struct timespec _ts; \
+			clock_gettime(CLOCK_REALTIME, &_ts); \
+			printf("[LVL:%08x][%6ld.%09ld] %s(L.%d) : ", level, _ts.tv_sec, _ts.tv_nsec, __func__, __LINE__); \
 			printf(__VA_ARGS__); \
 		} \
 }
 
-/*******************************************************
- * 列挙体／構造体
- *******************************************************/
-
-/*******************************************************
- * 関数宣言
- *******************************************************/
-
-/*******************************************************
- * 変数宣言
- *******************************************************/
-
-#endif /*__COMMON_H__ */
+#endif /*__TEMPLATE_COMMON_H__ */
