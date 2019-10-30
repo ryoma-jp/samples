b6e4a234a4ea4447f0b6fb22fd093248940c724d
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2019-10-31 06:46:07 +0900

    プログラムの使用方法表示関数(show_usage())をテンプレートとして追加

--- c/template/main.c
+++ c/template/main.c
@@ -7,6 +7,19 @@
 #include <common.h>
 
 /**
+ * @brief プログラムの使用方法の表示
+ * @return int 0固定
+ * @details プログラムの使用方法を表示する
+ */
+int show_usage()
+{
+	printf("<< Usage >>\n");
+	printf("  ./template\n");
+
+	return 0;
+}
+
+/**
  * @brief メイン関数
  * @param[in] argc 引数の数
  * @param[in] argv 引数
@@ -15,6 +28,7 @@
  */
 int main(int argc, char* argv[])
 {
+	show_usage();
 	template_print();
 
 	return 0;
