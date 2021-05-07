a81d11ab016b091e4d0553f6da039969c86279c7
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-05-08 05:40:14 +0900

    update: save prediction of test data for kaggle submission

--- python/tabnet_sample/main.py
+++ python/tabnet_sample/main.py
@@ -6,6 +6,7 @@
 import os
 import argparse
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from data_loader import titanic
 from tabnet.tabnet import TabNet
@@ -39,6 +40,7 @@ def main():
 	
 	os.makedirs(args.output_dir, exist_ok=True)
 	
+	# --- データセット読み込み ---
 	train_data, train_labels, test_data = titanic.load_titanic(args.dataset_dir)
 	print(train_data)
 	print(test_data)
@@ -50,18 +52,33 @@ def main():
 	x_train = np.array(x_train.values, dtype=np.float64)
 	y_train = np.array(y_train.values, dtype=np.float64)
 	
+	# --- TabNet学習 ---
 	tn_clf = TabNet()
 	scores, feature_importances = tn_clf.fit(x_train, y_train)
 	
 	print(feature_importances)
 	
+	# --- 特徴の重要度を出力 ---
 	plt.figure()
 	plt.barh(x_train_columns, feature_importances.mean(axis=0))
 	plt.title('feature_importance')
 	plt.tight_layout()
 	plt.savefig(os.path.join(args.output_dir, 'feature_importance.png'))
 	plt.close()
-
+	
+	# --- Kaggle submission用のcsvを出力 ---
+	x_test = test_data.drop(labels=["PassengerId", "Name"], axis=1, inplace=False)
+	x_test = np.array(x_test.values, dtype=np.float64)
+	prediction = tn_clf.inference(x_test)
+	print(prediction.shape)
+	print(prediction)
+	print(np.round(prediction.mean(axis=0)).astype(int))
+	
+	df_submission = pd.DataFrame({
+						"PassengerId": test_data["PassengerId"],
+						"Survived": np.round(prediction.mean(axis=0)).astype(int)})
+	df_submission.to_csv(os.path.join(args.output_dir, 'submission.csv'), index=False)
+	
 	return
 
 #---------------------------------
--- python/tabnet_sample/tabnet/tabnet.py
+++ python/tabnet_sample/tabnet/tabnet.py
@@ -5,6 +5,7 @@
 #---------------------------------
 import os
 import numpy as np
+import torch
 
 from pytorch_tabnet.tab_model import TabNetClassifier
 from sklearn.metrics import accuracy_score
@@ -26,12 +27,19 @@ class TabNet():
 		return
 	
 	def fit(self, x_train, y_train, kf_splits=5):
-		tabnet_params = dict(verbose=40)
-		tabnet_clf = TabNetClassifier(**tabnet_params)
 		kf = KFold(n_splits=kf_splits, shuffle=False)
 		scores = []
+		self.tabnet_clf_models = []
 		
 		for i, (train_index, val_index) in enumerate(kf.split(x_train, y_train)):
+			tabnet_params = dict(
+								verbose=40,
+								optimizer_fn=torch.optim.Adam,
+								optimizer_params=dict(lr=1e-2, weight_decay=1e-5),
+								scheduler_params=dict(max_lr=0.05, steps_per_epoch=x_train.shape[0] // 128, epochs=300),
+								scheduler_fn=torch.optim.lr_scheduler.OneCycleLR)
+			tabnet_clf = TabNetClassifier(**tabnet_params)
+			
 			x_tr = x_train[train_index]
 			x_val = x_train[val_index]
 			y_tr = y_train[train_index]
@@ -40,9 +48,11 @@ class TabNet():
 			tabnet_clf.fit(
 				x_tr, y_tr,
 				eval_set=[(x_val, y_val)],
+				eval_metric=['accuracy'],
 				patience=100,
-				max_epochs=300
+				max_epochs=500
 			)
+			self.tabnet_clf_models.append(tabnet_clf)
 			prediction = tabnet_clf.predict(x_val)
 			scores.append(accuracy_score(y_val, prediction))
 			
@@ -55,5 +65,13 @@ class TabNet():
 		print(np.mean(scores))
 		
 		return scores, feature_importances
-
+	
+	def inference(self, x_test):
+		for i, tabnet_clf in enumerate(self.tabnet_clf_models):
+			if (i == 0):
+				prediction = tabnet_clf.predict(x_test)
+			else:
+				prediction = np.vstack((prediction, tabnet_clf.predict(x_test)))
+		
+		return prediction
 
