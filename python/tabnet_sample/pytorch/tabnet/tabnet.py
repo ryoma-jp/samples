dbaf16a6a1a6f3322eac824b0dc0b7ee3d8b1a53
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-05-08 20:41:44 +0900

    update: parameter tuning

--- python/tabnet_sample/pytorch/tabnet/tabnet.py
+++ python/tabnet_sample/pytorch/tabnet/tabnet.py
@@ -26,18 +26,19 @@ class TabNet():
 	def __init__(self):
 		return
 	
-	def fit(self, x_train, y_train, kf_splits=5):
+	def fit(self, x_train, y_train, kf_splits=5, tabnet_params=None):
 		kf = KFold(n_splits=kf_splits, shuffle=False)
 		scores = []
 		self.tabnet_clf_models = []
 		
 		for i, (train_index, val_index) in enumerate(kf.split(x_train, y_train)):
-			tabnet_params = dict(
-								verbose=40,
-								optimizer_fn=torch.optim.Adam,
-								optimizer_params=dict(lr=1e-2, weight_decay=1e-5),
-								scheduler_params=dict(max_lr=0.05, steps_per_epoch=x_train.shape[0] // 128, epochs=300),
-								scheduler_fn=torch.optim.lr_scheduler.OneCycleLR)
+			if (tabnet_params is not None):
+				tabnet_params = dict(
+									verbose=40,
+									optimizer_fn=torch.optim.Adam,
+									optimizer_params=dict(lr=1e-2, weight_decay=1e-5),
+									scheduler_params=dict(max_lr=0.05, steps_per_epoch=x_train.shape[0] // 128, epochs=300),
+									scheduler_fn=torch.optim.lr_scheduler.OneCycleLR)
 			tabnet_clf = TabNetClassifier(**tabnet_params)
 			
 			x_tr = x_train[train_index]
