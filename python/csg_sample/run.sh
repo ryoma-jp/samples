4bc9cbd15a7a0c9466fb6847c55e9f5e874beaca
Ubuntu <ubuntu@ip-172-31-46-245.us-east-2.compute.internal>
2021-06-17 21:31:07 +0000

    update environments

--- python/csg_sample/run.sh
+++ python/csg_sample/run.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-git clone https://github.com/Dref360/spectral_metric.git
-cd spectral_metric
+if [ ! -e spectral_metric ]; then
+	git clone https://github.com/Dref360/spectral_metric.git
+	cd spectral_metric
+	patch -p1 < ../spectral_metric.patch
+else
+	cd spectral_metric
+fi
 
-wget "https://onedrive.live.com/download?cid=AB307638A9FB0EF9&resid=AB307638A9FB0EF9%21368&authkey=AC9YsfqB8u8f-nA" -O dataset.zip
-unzip dataset.zip
-DATASET_ROOT=$PWD
+export DATASET_ROOT=$PWD
+if [ ! -e "${DATASET_ROOT}/Datasets" ]; then
+	wget "https://onedrive.live.com/download?cid=AB307638A9FB0EF9&resid=AB307638A9FB0EF9%21368&authkey=AC9YsfqB8u8f-nA" -O dataset.zip
+	unzip dataset.zip
+fi
 
 python3 ./main.py mnist cifar10 --embd cnn_embd --tsne --make_graph
 
