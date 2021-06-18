1797bf13c32a1c6ebeeb783be0f990904ca14257
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-06-18 21:52:02 +0900

    update environment

--- python/csg_sample/run.sh
+++ python/csg_sample/run.sh
@@ -9,10 +9,5 @@ else
 fi
 
 export DATASET_ROOT=$PWD
-if [ ! -e "${DATASET_ROOT}/Datasets" ]; then
-	wget "https://onedrive.live.com/download?cid=AB307638A9FB0EF9&resid=AB307638A9FB0EF9%21368&authkey=AC9YsfqB8u8f-nA" -O dataset.zip
-	unzip dataset.zip
-fi
-
 python3 ./main.py mnist cifar10 --embd cnn_embd --tsne --make_graph
 
