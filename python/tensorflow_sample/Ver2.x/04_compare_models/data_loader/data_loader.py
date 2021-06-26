7bda8147528e9bac6933350685a54d91f8a096ef
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-06-27 05:47:06 +0900

    add: compare models (baseline)

---
+++ python/tensorflow_sample/Ver2.x/04_compare_models/data_loader/data_loader.py
@@ -0,0 +1,133 @@
+#! -*- coding: utf-8 -*-
+
+#---------------------------------
+# モジュールのインポート
+#---------------------------------
+import os
+import numpy as np
+
+#---------------------------------
+# クラス; データ取得基底クラス
+#---------------------------------
+class DataLoader():
+	# --- コンストラクタ ---
+	def __init__(self):
+		return
+	
+	# --- ラベルインデックス取得 ---
+	def get_label_index(self, label, one_hot=True):
+		if (one_hot):
+			label = np.argmax(label, axis=1)
+		n_category = max(label)+1
+		
+		return np.array([np.arange(len(label))[label==i] for i in range(n_category)])
+
+#---------------------------------
+# クラス; CIFAR-10データセット取得
+#---------------------------------
+class DataLoaderCIFAR10(DataLoader):
+	def __init__(self, dataset_dir, flatten=False, one_hot=False):
+		def unpickle(file):
+			import pickle
+			with open(file, 'rb') as fo:
+				dict = pickle.load(fo, encoding='bytes')
+			return dict
+		
+		# --- initialize super class ---
+		super().__init__()
+
+		# --- load training data ---
+		train_data_list = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
+		dict_data = unpickle(os.path.join(dataset_dir, train_data_list[0]))
+		train_images = dict_data[b'data']
+		train_labels = dict_data[b'labels'].copy()
+		for train_data in train_data_list[1:]:
+			dict_data = unpickle(os.path.join(dataset_dir, train_data))
+			train_images = np.vstack((train_images, dict_data[b'data']))
+			train_labels = np.hstack((train_labels, dict_data[b'labels']))
+		
+		# --- load test data ---
+		test_data = "test_batch"
+		dict_data = unpickle(os.path.join(dataset_dir, test_data))
+		test_images = dict_data[b'data']
+		test_labels = dict_data[b'labels'].copy()
+		
+		# --- transpose: [N, C, H, W] -> [N, H, W, C] ---
+		self.train_images = train_images.reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
+		self.test_images = test_images.reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
+		
+		# --- labels ---
+		if (one_hot):
+			identity = np.eye(10, dtype=np.int)
+			self.train_labels = np.array([identity[i] for i in train_labels])
+			self.test_labels = np.array([identity[i] for i in test_labels])
+		else:
+			self.train_labels = np.array(train_labels)
+			self.test_labels = np.array(test_labels)
+		
+		# --- 出力次元数を保持 ---
+		self.output_dims = 10
+		
+		return
+		
+#---------------------------------
+# クラス; MNISTデータセット取得
+#---------------------------------
+class DataLoaderMNIST(DataLoader):
+	# --- コンストラクタ ---
+	def __init__(self, dataset_dir, flatten=False, one_hot=False):
+		# --- initialize super class ---
+		super().__init__()
+		
+		# --- load training data ---
+		f = open(os.path.join(dataset_dir, 'train-images-idx3-ubyte'))
+		byte_data = np.fromfile(f, dtype=np.uint8)
+		
+		n_items = (byte_data[4] << 24) | (byte_data[5] << 16) | (byte_data[6] << 8) | (byte_data[7])
+		img_h = (byte_data[8] << 24) | (byte_data[9] << 16) | (byte_data[10] << 8) | (byte_data[11])
+		img_w = (byte_data[12] << 24) | (byte_data[13] << 16) | (byte_data[14] << 8) | (byte_data[15])
+		
+		if (flatten):
+			self.train_images = byte_data[16:].reshape(n_items, -1)
+		else:
+			self.train_images = byte_data[16:].reshape(n_items, img_h, img_w, 1)
+		
+		# --- load training label ---
+		f = open(os.path.join(dataset_dir, 'train-labels-idx1-ubyte'))
+		byte_data = np.fromfile(f, dtype=np.uint8)
+		
+		n_items = (byte_data[4] << 24) | (byte_data[5] << 16) | (byte_data[6] << 8) | (byte_data[7])
+		
+		self.train_labels = byte_data[8:]
+		if (one_hot):
+			identity = np.eye(10, dtype=np.int)
+			self.train_labels = np.array([identity[i] for i in self.train_labels])
+		
+		# --- load test data ---
+		f = open(os.path.join(dataset_dir, 't10k-images-idx3-ubyte'))
+		byte_data = np.fromfile(f, dtype=np.uint8)
+		
+		n_items = (byte_data[4] << 24) | (byte_data[5] << 16) | (byte_data[6] << 8) | (byte_data[7])
+		img_h = (byte_data[8] << 24) | (byte_data[9] << 16) | (byte_data[10] << 8) | (byte_data[11])
+		img_w = (byte_data[12] << 24) | (byte_data[13] << 16) | (byte_data[14] << 8) | (byte_data[15])
+		
+		if (flatten):
+			self.test_images = byte_data[16:].reshape(n_items, -1)
+		else:
+			self.test_images = byte_data[16:].reshape(n_items, img_h, img_w, 1)
+		
+		# --- load test label ---
+		f = open(os.path.join(dataset_dir, 't10k-labels-idx1-ubyte'))
+		byte_data = np.fromfile(f, dtype=np.uint8)
+		
+		n_items = (byte_data[4] << 24) | (byte_data[5] << 16) | (byte_data[6] << 8) | (byte_data[7])
+		
+		self.test_labels = byte_data[8:]
+		if (one_hot):
+			self.test_labels = np.array([identity[i] for i in self.test_labels])
+		
+		# --- 出力次元数を保持 ---
+		self.output_dims = 10
+		
+		return
+	
---
+++ python/tensorflow_sample/Ver2.x/04_compare_models/main.py
@@ -0,0 +1,108 @@
+#! -*- coding: utf-8 -*-
+
+#---------------------------------
+# モジュールのインポート
+#---------------------------------
+import os
+import argparse
+
+from data_loader.data_loader import DataLoaderMNIST
+from data_loader.data_loader import DataLoaderCIFAR10
+
+from trainer.trainer import TrainerMLP, TrainerCNN, TrainerResNet
+
+#---------------------------------
+# 定数定義
+#---------------------------------
+
+#---------------------------------
+# 関数
+#---------------------------------
+def ArgParser():
+	parser = argparse.ArgumentParser(description='TensorFlowの学習実装サンプル\n'
+													'  * No.01: MNISTデータセットを用いた全結合NN学習サンプル\n'
+													'  * No.02: CIFAR-10データセットを用いたCNN学習サンプル\n'
+													'  * No.03: CIFAR-10データセットを用いたResNet学習サンプル',
+				formatter_class=argparse.RawTextHelpFormatter)
+
+	# --- 引数を追加 ---
+	parser.add_argument('--data_type', dest='data_type', type=str, default='CIFAR-10', required=False, \
+			help='データ種別(MNIST, CIFAR-10)')
+	parser.add_argument('--dataset_dir', dest='dataset_dir', type=str, default=None, required=True, \
+			help='データセットディレクトリ')
+	parser.add_argument('--model_type', dest='model_type', type=str, default='ResNet', required=False, \
+			help='モデル種別(MLP, SimpleCNN, SimpleResNet)')
+	parser.add_argument('--result_dir', dest='result_dir', type=str, default='./result', required=False, \
+			help='学習結果の出力先ディレクトリ')
+
+	args = parser.parse_args()
+
+	return args
+
+def main():
+	# --- 引数処理 ---
+	args = ArgParser()
+	print('[INFO] Arguments')
+	print('  * args.data_type = {}'.format(args.data_type))
+	print('  * args.dataset_dir = {}'.format(args.dataset_dir))
+	print('  * args.model_type = {}'.format(args.model_type))
+	print('  * args.result_dir = {}'.format(args.result_dir))
+	
+	if (args.data_type == "MNIST"):
+		dataset = DataLoaderMNIST(args.dataset_dir)
+		print(dataset.train_images.shape)
+		print(dataset.train_labels.shape)
+		print(dataset.test_images.shape)
+		print(dataset.test_labels.shape)
+		
+		x_train = dataset.train_images / 255
+		y_train = dataset.train_labels
+		x_test = dataset.test_images / 255
+		y_test = dataset.test_labels
+		output_dims = dataset.output_dims
+	elif (args.data_type == "CIFAR-10"):
+		dataset = DataLoaderCIFAR10(args.dataset_dir)
+		print(dataset.train_images.shape)
+		print(dataset.train_labels.shape)
+		print(dataset.test_images.shape)
+		print(dataset.test_labels.shape)
+		
+		x_train = dataset.train_images / 255
+		y_train = dataset.train_labels
+		x_test = dataset.test_images / 255
+		y_test = dataset.test_labels
+		output_dims = dataset.output_dims
+	else:
+		print('[ERROR] Unknown data_type: {}'.format(args.data_type))
+		quit()
+	
+	if (args.model_type == 'MLP'):
+		trainer = TrainerMLP(dataset.train_images.shape[1:], output_dir=args.result_dir)
+		trainer.fit(x_train, y_train, x_test=x_test, y_test=y_test)
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	elif (args.model_type == 'SimpleCNN'):
+		trainer = TrainerCNN(dataset.train_images.shape[1:], output_dir=args.result_dir)
+		trainer.fit(x_train, y_train, x_test=x_test, y_test=y_test)
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	elif (args.model_type == 'SimpleResNet'):
+		trainer = TrainerResNet(dataset.train_images.shape[1:], output_dims, output_dir=args.result_dir)
+		trainer.fit(x_train, y_train, x_test=x_test, y_test=y_test)
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	else:
+		print('[ERROR] Unknown model_type: {}'.format(args.model_type))
+		quit()
+
+	return
+
+#---------------------------------
+# メイン処理
+#---------------------------------
+if __name__ == '__main__':
+	main()
+
