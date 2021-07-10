9a206d21f851de230811035d874aa601ba960444
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-07-11 08:33:28 +0900

    add: 07_parameter_tunings (baseline, overfitting)

---
+++ python/tensorflow_sample/Ver2.x/07_parameter_tunings/main.py
@@ -0,0 +1,155 @@
+#! -*- coding: utf-8 -*-
+
+#---------------------------------
+# モジュールのインポート
+#---------------------------------
+import os
+import argparse
+
+from data_loader.data_loader import DataLoaderMNIST
+from data_loader.data_loader import DataLoaderCIFAR10
+
+from trainer.trainer import TrainerMLP, TrainerCNN, TrainerResNet
+
+#---------------------------------
+# 定数定義
+#---------------------------------
+
+#---------------------------------
+# 関数
+#---------------------------------
+def ArgParser():
+	parser = argparse.ArgumentParser(description='TensorFlowの学習実装サンプル\n'
+													'  * No.01: MNISTデータセットを用いた全結合NN学習サンプル\n'
+													'  * No.02: CIFAR-10データセットを用いたCNN学習サンプル\n'
+													'  * No.03: CIFAR-10データセットを用いたResNet学習サンプル',
+				formatter_class=argparse.RawTextHelpFormatter)
+
+	# --- 引数を追加 ---
+	parser.add_argument('--data_type', dest='data_type', type=str, default='CIFAR-10', required=False, \
+			help='データ種別(MNIST, CIFAR-10)')
+	parser.add_argument('--dataset_dir', dest='dataset_dir', type=str, default=None, required=True, \
+			help='データセットディレクトリ')
+	parser.add_argument('--model_type', dest='model_type', type=str, default='ResNet', required=False, \
+			help='モデル種別(MLP, SimpleCNN, SimpleResNet)')
+	parser.add_argument('--data_augmentation', dest='data_augmentation', action='store_true', default=False, required=False, \
+			help='Data Augmentation有無を指定(True: あり，False:(default)：なし)')
+	parser.add_argument('--optimizer', dest='optimizer', type=str, default='adam', required=False, \
+			help='Optimizer(adam(default), sgd, adam_lrs, sgd, lrs)\n'
+					'  * lrs: Learning Rate Scheduler')
+	parser.add_argument('--result_dir', dest='result_dir', type=str, default='./result', required=False, \
+			help='学習結果の出力先ディレクトリ')
+
+	args = parser.parse_args()
+
+	return args
+
+def main():
+	# --- NumPy配列形状表示 ---
+	def print_ndarray_shape(ndarr):
+		if (ndarr is not None):
+			print(ndarr.shape)
+		else:
+			pass
+		return
+		
+	# --- 引数処理 ---
+	args = ArgParser()
+	print('[INFO] Arguments')
+	print('  * args.data_type = {}'.format(args.data_type))
+	print('  * args.dataset_dir = {}'.format(args.dataset_dir))
+	print('  * args.model_type = {}'.format(args.model_type))
+	print('  * args.data_augmentation = {}'.format(args.data_augmentation))
+	print('  * args.optimizer = {}'.format(args.optimizer))
+	print('  * args.result_dir = {}'.format(args.result_dir))
+	
+	if (args.data_type == "MNIST"):
+		dataset = DataLoaderMNIST(args.dataset_dir, validation_split=0.2)
+		print_ndarray_shape(dataset.train_images)
+		print_ndarray_shape(dataset.train_labels)
+		print_ndarray_shape(dataset.validation_images)
+		print_ndarray_shape(dataset.validation_labels)
+		print_ndarray_shape(dataset.test_images)
+		print_ndarray_shape(dataset.test_labels)
+		
+		if (dataset.train_images is not None):
+			x_train = dataset.train_images / 255
+		else:
+			x_train = None
+		y_train = dataset.train_labels
+		if (dataset.validation_images is not None):
+			x_val = dataset.validation_images / 255
+		else:
+			x_val = None
+		y_val = dataset.validation_labels
+		if (dataset.test_images is not None):
+			x_test = dataset.test_images / 255
+		else:
+			x_test = None
+		y_test = dataset.test_labels
+		output_dims = dataset.output_dims
+	elif (args.data_type == "CIFAR-10"):
+		dataset = DataLoaderCIFAR10(args.dataset_dir, validation_split=0.2)
+		print_ndarray_shape(dataset.train_images)
+		print_ndarray_shape(dataset.train_labels)
+		print_ndarray_shape(dataset.validation_images)
+		print_ndarray_shape(dataset.validation_labels)
+		print_ndarray_shape(dataset.test_images)
+		print_ndarray_shape(dataset.test_labels)
+		
+		if (dataset.train_images is not None):
+			x_train = dataset.train_images / 255
+		else:
+			x_train = None
+		y_train = dataset.train_labels
+		if (dataset.validation_images is not None):
+			x_val = dataset.validation_images / 255
+		else:
+			x_val = None
+		y_val = dataset.validation_labels
+		if (dataset.test_images is not None):
+			x_test = dataset.test_images / 255
+		else:
+			x_test = None
+		y_test = dataset.test_labels
+		output_dims = dataset.output_dims
+	else:
+		print('[ERROR] Unknown data_type: {}'.format(args.data_type))
+		quit()
+	
+	if (args.model_type == 'MLP'):
+		trainer = TrainerMLP(dataset.train_images.shape[1:], output_dir=args.result_dir, optimizer=args.optimizer)
+		trainer.fit(x_train, y_train, x_val=x_val, y_val=y_val, x_test=x_test, y_test=y_test,
+			da_enable=args.data_augmentation)
+		trainer.save_model()
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	elif (args.model_type == 'SimpleCNN'):
+		trainer = TrainerCNN(dataset.train_images.shape[1:], output_dir=args.result_dir, optimizer=args.optimizer)
+		trainer.fit(x_train, y_train, x_val=x_val, y_val=y_val, x_test=x_test, y_test=y_test,
+			da_enable=args.data_augmentation)
+		trainer.save_model()
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	elif (args.model_type == 'SimpleResNet'):
+		trainer = TrainerResNet(dataset.train_images.shape[1:], output_dims, output_dir=args.result_dir, optimizer=args.optimizer)
+		trainer.fit(x_train, y_train, x_val=x_val, y_val=y_val, x_test=x_test, y_test=y_test,
+			da_enable=args.data_augmentation)
+		trainer.save_model()
+		
+		predictions = trainer.predict(x_test)
+		print('\nPredictions(shape): {}'.format(predictions.shape))
+	else:
+		print('[ERROR] Unknown model_type: {}'.format(args.model_type))
+		quit()
+
+	return
+
+#---------------------------------
+# メイン処理
+#---------------------------------
+if __name__ == '__main__':
+	main()
+
---
+++ python/tensorflow_sample/Ver2.x/07_parameter_tunings/run.sh
@@ -0,0 +1,163 @@
+#! /bin/bash
+
+# --- Fixed parameters ---
+DATASET_DIR="./dataset"
+LIB_DIR="/work/lib"
+mkdir -p ${DATASET_DIR}
+
+# --- Prepare dataset ---
+#  * MNIST
+#  * CIFAR-10
+dataset_dir="${DATASET_DIR}/mnist"
+if [ ! -e ${dataset_dir} ]; then
+	mkdir -p ${dataset_dir}
+	cd ${dataset_dir}
+	wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz &
+	wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz &
+	wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz &
+	wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+	wait
+	gunzip train-images-idx3-ubyte.gz &
+	gunzip train-labels-idx1-ubyte.gz &
+	gunzip t10k-images-idx3-ubyte.gz &
+	gunzip t10k-labels-idx1-ubyte.gz &
+	wait
+	cd ../..
+fi
+
+dataset_dir="${DATASET_DIR}/cifar-10-batches-py"
+if [ ! -e ${DATASET_DIR}/cifar-10-python.tar.gz ]; then
+	cd ${DATASET_DIR}
+	wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+	tar -zxf cifar-10-python.tar.gz
+	cd ..
+fi
+
+# --- Training ---
+#  * OUTPUT_DIRで指定のトップディレクトリが存在する場合は学習しない
+echo `pwd`
+OUTPUT_DIR="./output"
+DATA_TYPE_LIST=("CIFAR-10")
+MODEL_TYPE_LIST=("SimpleResNet")
+DATA_AUG_LIST=("False")
+OPTIMIZER_LIST=("momentum" "adam" "sgd_lrs")
+
+if [ ! -e ${OUTPUT_DIR} ]; then
+	for DATA_TYPE in ${DATA_TYPE_LIST[@]}
+	do
+		if [ ${DATA_TYPE} = "MNIST" ]; then
+			dataset_dir="${DATASET_DIR}/mnist"
+		elif [ ${DATA_TYPE} = "CIFAR-10" ]; then
+			dataset_dir="${DATASET_DIR}/cifar-10-batches-py"
+		else
+			echo "[ERROR] Unknown DATA_TYPE; ${DATA_TYPE}"
+			exit
+		fi
+		
+		for MODEL_TYPE in ${MODEL_TYPE_LIST[@]}
+		do
+			for DATA_AUG in ${DATA_AUG_LIST[@]}
+			do
+				for OPTIMIZER in ${OPTIMIZER_LIST[@]}
+				do
+					echo "[Training Conditions]"
+					echo "  * MODEL_TYPE=${MODEL_TYPE}"
+					echo "  * DATA_TYPE=${DATA_TYPE}"
+					echo "  * DATA_AUG=${DATA_AUG}"
+					echo "  * OPTIMIZER=${OPTIMIZER}"
+					
+					model_dir="${OUTPUT_DIR}/model/${MODEL_TYPE}_${DATA_TYPE}_DA-${DATA_AUG}_OPT-${OPTIMIZER}"
+					mkdir -p ${model_dir}
+					
+					if [ ${DATA_AUG} = "True" ]; then
+						python3 main.py --data_type ${DATA_TYPE} \
+							--dataset_dir ${dataset_dir} \
+							--model_type ${MODEL_TYPE} \
+							--data_augmentation \
+							--optimizer ${OPTIMIZER} \
+							--result_dir ${model_dir}
+					else
+						python3 main.py --data_type ${DATA_TYPE} \
+							--dataset_dir ${dataset_dir} \
+							--model_type ${MODEL_TYPE} \
+							--optimizer ${OPTIMIZER} \
+							--result_dir ${model_dir}
+					fi
+				done
+			done
+		done
+	done
+fi
+
+# --- 出力するグラフのサイズ[inch] ---
+fig_size="15,5"
+
+# --- モデル定義 ---
+MLP_CIFAR10_DATrue="MLP_CIFAR-10_DA-True_OPT-adam"
+MLP_CIFAR10_DAFalse="MLP_CIFAR-10_DA-False_OPT-adam"
+MLP_MNIST_DATrue="MLP_MNIST_DA-True_OPT-adam"
+MLP_MNIST_DAFalse="MLP_MNIST_DA-False_OPT-adam"
+SimpleCNN_CIFAR10_DATrue="SimpleCNN_CIFAR-10_DA-True_OPT-adam"
+SimpleCNN_CIFAR10_DAFalse="SimpleCNN_CIFAR-10_DA-False_OPT-adam"
+SimpleCNN_MNIST_DATrue="SimpleCNN_MNIST_DA-True_OPT-adam"
+SimpleCNN_MNIST_DAFalse="SimpleCNN_MNIST_DA-False_OPT-adam"
+SimpleResNet_CIFAR10_DATrue="SimpleResNet_CIFAR-10_DA-True_OPT-adam"
+SimpleResNet_CIFAR10_DAFalse="SimpleResNet_CIFAR-10_DA-False_OPT-adam"
+SimpleResNet_MNIST_DATrue="SimpleResNet_MNIST_DA-True_OPT-adam"
+SimpleResNet_MNIST_DAFalse="SimpleResNet_MNIST_DA-False_OPT-adam"
+
+MLP_CIFAR10_DATrue_OPTsgd="MLP_CIFAR-10_DA-True_OPT-sgd"
+MLP_CIFAR10_DAFalse_OPTsgd="MLP_CIFAR-10_DA-False_OPT-sgd"
+MLP_MNIST_DATrue_OPTsgd="MLP_MNIST_DA-True_OPT-sgd"
+MLP_MNIST_DAFalse_OPTsgd="MLP_MNIST_DA-False_OPT-sgd"
+SimpleCNN_CIFAR10_DATrue_OPTsgd="SimpleCNN_CIFAR-10_DA-True_OPT-sgd"
+SimpleCNN_CIFAR10_DAFalse_OPTsgd="SimpleCNN_CIFAR-10_DA-False_OPT-sgd"
+SimpleCNN_MNIST_DATrue_OPTsgd="SimpleCNN_MNIST_DA-True_OPT-sgd"
+SimpleCNN_MNIST_DAFalse_OPTsgd="SimpleCNN_MNIST_DA-False_OPT-sgd"
+SimpleResNet_CIFAR10_DATrue_OPTsgd="SimpleResNet_CIFAR-10_DA-True_OPT-sgd"
+SimpleResNet_CIFAR10_DAFalse_OPTsgd="SimpleResNet_CIFAR-10_DA-False_OPT-sgd"
+SimpleResNet_MNIST_DATrue_OPTsgd="SimpleResNet_MNIST_DA-True_OPT-sgd"
+SimpleResNet_MNIST_DAFalse_OPTsgd="SimpleResNet_MNIST_DA-False_OPT-sgd"
+
+MLP_CIFAR10_DATrue_OPTadamlrs="MLP_CIFAR-10_DA-True_OPT-adam_lrs"
+MLP_CIFAR10_DAFalse_OPTadamlrs="MLP_CIFAR-10_DA-False_OPT-adam_lrs"
+MLP_MNIST_DATrue_OPTadamlrs="MLP_MNIST_DA-True_OPT-adam_lrs"
+MLP_MNIST_DAFalse_OPTadamlrs="MLP_MNIST_DA-False_OPT-adam_lrs"
+SimpleCNN_CIFAR10_DATrue_OPTadamlrs="SimpleCNN_CIFAR-10_DA-True_OPT-adam_lrs"
+SimpleCNN_CIFAR10_DAFalse_OPTadamlrs="SimpleCNN_CIFAR-10_DA-False_OPT-adam_lrs"
+SimpleCNN_MNIST_DATrue_OPTadamlrs="SimpleCNN_MNIST_DA-True_OPT-adam_lrs"
+SimpleCNN_MNIST_DAFalse_OPTadamlrs="SimpleCNN_MNIST_DA-False_OPT-adam_lrs"
+SimpleResNet_CIFAR10_DATrue_OPTadamlrs="SimpleResNet_CIFAR-10_DA-True_OPT-adam_lrs"
+SimpleResNet_CIFAR10_DAFalse_OPTadamlrs="SimpleResNet_CIFAR-10_DA-False_OPT-adam_lrs"
+SimpleResNet_MNIST_DATrue_OPTadamlrs="SimpleResNet_MNIST_DA-True_OPT-adam_lrs"
+SimpleResNet_MNIST_DAFalse_OPTadamlrs="SimpleResNet_MNIST_DA-False_OPT-adam_lrs"
+
+MLP_CIFAR10_DATrue_OPTsgdlrs="MLP_CIFAR-10_DA-True_OPT-sgd_lrs"
+MLP_CIFAR10_DAFalse_OPTsgdlrs="MLP_CIFAR-10_DA-False_OPT-sgd_lrs"
+MLP_MNIST_DATrue_OPTsgdlrs="MLP_MNIST_DA-True_OPT-sgd_lrs"
+MLP_MNIST_DAFalse_OPTsgdlrs="MLP_MNIST_DA-False_OPT-sgd_lrs"
+SimpleCNN_CIFAR10_DATrue_OPTsgdlrs="SimpleCNN_CIFAR-10_DA-True_OPT-sgd_lrs"
+SimpleCNN_CIFAR10_DAFalse_OPTsgdlrs="SimpleCNN_CIFAR-10_DA-False_OPT-sgd_lrs"
+SimpleCNN_MNIST_DATrue_OPTsgdlrs="SimpleCNN_MNIST_DA-True_OPT-sgd_lrs"
+SimpleCNN_MNIST_DAFalse_OPTsgdlrs="SimpleCNN_MNIST_DA-False_OPT-sgd_lrs"
+SimpleResNet_CIFAR10_DATrue_OPTsgdlrs="SimpleResNet_CIFAR-10_DA-True_OPT-sgd_lrs"
+SimpleResNet_CIFAR10_DAFalse_OPTsgdlrs="SimpleResNet_CIFAR-10_DA-False_OPT-sgd_lrs"
+SimpleResNet_MNIST_DATrue_OPTsgdlrs="SimpleResNet_MNIST_DA-True_OPT-sgd_lrs"
+SimpleResNet_MNIST_DAFalse_OPTsgdlrs="SimpleResNet_MNIST_DA-False_OPT-sgd_lrs"
+
+SimpleResNet_CIFAR10_DAFalse_OPTmomentum="SimpleResNet_CIFAR-10_DA-False_OPT-momentum"
+
+# --- Compare models(ALL) ---
+metrics_list=\
+"${OUTPUT_DIR}/model/${SimpleResNet_CIFAR10_DAFalse_OPTmomentum}/metrics/metrics.csv,"\
+"${OUTPUT_DIR}/model/${SimpleResNet_CIFAR10_DAFalse}/metrics/metrics.csv,"\
+"${OUTPUT_DIR}/model/${SimpleResNet_CIFAR10_DAFalse_OPTsgdlrs}/metrics/metrics.csv"
+metrics_names=\
+"${SimpleResNet_CIFAR10_DAFalse_OPTmomentum},"\
+"${SimpleResNet_CIFAR10_DAFalse},"\
+"${SimpleResNet_CIFAR10_DAFalse_OPTsgdlrs}"
+output_dir="${OUTPUT_DIR}/metrics_graph"
+
+python3 tools/create_metrics_graph/create_metrics_graph.py --metrics_list ${metrics_list} --metrics_names ${metrics_names} --fig_size ${fig_size} --output_dir ${output_dir}
+
+
