66d3d5d2a6885e264ad6666e6cbe1da516486313
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-10-23 09:56:41 +0900

    update: add sample of numpy.digitize

--- python/numpy_sample/docker/docker_run.sh
+++ python/numpy_sample/docker/docker_run.sh
@@ -8,6 +8,6 @@ echo ${WORK_ROOT}
 
 CONTAINER="numpy_sample/scipy-notebook:latest"
 
-docker run -v "${WORK_ROOT}/work:/home/jovyan/work" -p 10000:8888 ${CONTAINER}
+docker run -v "${WORK_ROOT}:/home/jovyan/work" -p 10000:8888 ${CONTAINER}
 
 
