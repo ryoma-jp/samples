a018f42db57bfe5cc8223112beb46f4ed1fbecbb
Ryoichi Matsumoto <iyo.rotomus@gmail.com>
2021-12-18 07:56:17 +0900

    update: add callbacks for TensorBoard

--- python/automated_machine_learning/docker/docker_run.sh
+++ python/automated_machine_learning/docker/docker_run.sh
@@ -8,6 +8,10 @@ echo ${WORK_ROOT}
 
 CONTAINER="automated_machine_learning/jupyter:latest"
 
-docker run --gpus all --rm -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v "${WORK_ROOT}/:/tf/work" -p 10001:8888 ${CONTAINER}
+docker run --gpus all --rm -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+	-v "${WORK_ROOT}/:/tf/work" \
+	-p 10001:8888 \
+	-p 10002:35000 \
+	${CONTAINER}
 
 
