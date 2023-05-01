7592f58ea91d0f1291648a44029999ac308473e0
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2023-05-01 17:59:51 +0900

    update the file download sample program

--- python/file_download/README.md
+++ python/file_download/README.md
@@ -2,11 +2,31 @@
 
 サイズの小さなファイルは``requests.get(url).content``でダウンロードするのが通常だが，サイズが大きなファイルば分割してダウンロードする必要がある．
 
+ダウンロード対象として，機械学習で使用する[COCO Dataset](https://cocodataset.org/#download)をダウンロードする．  
+一括ダウンロードとして``2017 Train/Val annotations [241MB]``を，分割ダウンロードとして``2017 Train images [118K/18GB]``を例に挙げる．
+
 ## 使い方
 
+### 前準備
+
 ```
-$ docker-compose up
+$ docker-compose build
+$ docker-compose up -d
+$ docker-compose exec --user $UID file_download bash
+$ cd /work
 ```
+
+### 実行
+
+#### 一括ダウンロード
+
+```
+$ python3 file_download.py --file_size small
+```
+
+#### 分割ダウンロード
+
+T.B.D
 
 ## 参照
 
--- python/file_download/file_download.py
+++ python/file_download/file_download.py
@@ -1,8 +1,47 @@
 
+import os
+import argparse
+import requests
 
+from pathlib import Path
 
+def ArgParser():
+    """ArgParser
+    
+    Argument parser
+    """
+    parser = argparse.ArgumentParser(description='ファイルをダウンロードするプログラムのサンプル',
+                formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('--file_size', dest='file_size', type=str, default='small', choices=['small', 'big'], \
+            help='ダウンロードするファイルの大きさを選択する\n'
+                 '    - small: ``2017 Train/Val annotations``を一括でダウンロードする\n'
+                 '    - big  : ``2017 Train images``を分割してダウンロードする\n')
+    parser.add_argument('--download_dir', dest='download_dir', type=str, default='downloads', \
+            help='ダウンロードしたファイルを格納するディレクトリを指定する')
+
+    args = parser.parse_args()
+
+    return args
 def main():
-    print('test')
+    args = ArgParser()
+    print(f'args.file_size : {args.file_size}')
+    print(f'args.download_dir : {args.download_dir}')
+    
+    os.makedirs(args.download_dir, exist_ok=True)
+    
+    if (args.file_size == 'small'):
+        url = 'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
+        #url = 'http://images.cocodataset.org/zips/train2017.zip'
+        
+        save_file = Path(args.download_dir, Path(url).name)
+        content = requests.get(url).content
+        
+        with open(save_file, mode='wb') as f:
+            f.write(content)
+    else:
+        print('T.B.D')
+    
 
 
 # --- main routine ---
