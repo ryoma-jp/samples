a2adefac8bc2b02dc0d4e284b85d1b526af7a4c8
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2023-06-24 14:00:25 +0900

    save feature map

--- python/tensorflow_sample/Ver2.x/12_get_features/simple_cnn_saved_model.py
+++ python/tensorflow_sample/Ver2.x/12_get_features/simple_cnn_saved_model.py
@@ -1,4 +1,6 @@
 
+import os
+import cv2
 import logging
 import numpy as np
 import tensorflow as tf
@@ -17,8 +19,10 @@ def main():
     
     # --- Get features ---
     #  * get output attributes of 'Conv2D' and 'Dense'
-    feature_attr = ['Conv2D', 'Dense']
+    inputs = model.input
+    feature_attr = ['Conv2D']
     feature_list = []
+    feature_list.append(model.output)
     for i, layer in enumerate(model.layers):
         if (layer.__class__.__name__ in feature_attr):
             feature_list.append(layer.output)
@@ -27,6 +31,9 @@ def main():
             for j, func_layer in enumerate(layer_config['layers']):
                 if (func_layer['class_name'] in feature_attr):
                     feature_list.append(layer.layers[j].output)
+    
+    # --- Build model to get feature maps ---
+    model = tf.keras.models.Model(inputs=inputs, outputs=feature_list)
     
     # --- Show feature_list ---
     logging.info(feature_list)
@@ -39,15 +46,36 @@ def main():
     logging.info(f'  * test_images.shape: {test_images.shape}')
     logging.info(f'  * test_labels.shape: {test_labels.shape}')
     
-    # --- Preciction ---
-    input_tensor = test_images / 255.
-    prediction = model.predict(input_tensor)
+    # --- Prediction ---
+    test_num = 10
+    input_tensor = test_images[0:test_num] / 255.
+    output_tensor = model.predict(input_tensor)
+    prediction = output_tensor[0]
     logging.info(f'prediction.shape: {prediction.shape}')
     logging.info(f'prediction.argmax: {prediction.argmax(axis=1)}')
     
     # --- Calc accuracy ---
-    acc = accuracy_score(test_labels.argmax(axis=1), prediction.argmax(axis=1))
+    acc = accuracy_score(test_labels[0:test_num].argmax(axis=1), prediction.argmax(axis=1))
     logging.info(f'accuracy: {acc}')
+    
+    # --- Get feature maps ---
+    for i, feature in enumerate(output_tensor[1:]):
+        logging.info(f'Feature #{i}: {feature.shape}')
+        
+        for img_no, feature_per_img in enumerate(feature):
+            # --- convert to [0 .. 255] to save image ---
+            min = feature_per_img.min()
+            max = feature_per_img.max()
+            feature_per_img_norm = ((feature_per_img - min) / (max - min) * 255).astype(np.uint8)
+            
+            for filter_no, feature_per_filter in enumerate(feature_per_img_norm.transpose(2, 0, 1)):
+                feature_map_dir = Path('feature_maps', f'img{img_no}')
+                feature_file = f'feature{i}_filter{filter_no}.png'
+                save_file = str(Path(feature_map_dir, feature_file))
+                
+                os.makedirs(feature_map_dir, exist_ok=True)
+                cv2.imwrite(save_file, feature_per_filter)
+                
     
 if __name__=='__main__':
     main()
