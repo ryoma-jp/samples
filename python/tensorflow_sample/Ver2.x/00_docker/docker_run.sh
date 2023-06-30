4ff701f82a91e2dfa5afa07a753a5c156d4453e7
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2023-06-30 17:54:20 +0900

    update environment

--- python/tensorflow_sample/Ver2.x/00_docker/docker_run.sh
+++ python/tensorflow_sample/Ver2.x/00_docker/docker_run.sh
@@ -6,7 +6,7 @@
 WORK_ROOT="${PWD}/.."
 echo ${WORK_ROOT}
 
-CONTAINER="tf_sample_v2/tensorflow:21.03-tf2-py3"
+CONTAINER="tf_sample_v2/tensorflow:ubuntu22.04"
 
 docker run --gpus all --rm -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v "${WORK_ROOT}:/work" -p 6006:6006 ${CONTAINER}
 
