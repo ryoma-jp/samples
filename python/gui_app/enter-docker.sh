b153e73b355f64c46df0f969afe1b09947fdfe1b
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2024-11-17 09:24:55 +0900

    bugfix for WSL

--- python/gui_app/enter-docker.sh
+++ python/gui_app/enter-docker.sh
@@ -2,8 +2,14 @@
 
 DOCKER_IMAGE="python_gui_app:latest"
 
+MOUNT="-v `pwd`:`pwd`"
+if [ -d "/tmp/.X11-unix" ]; then
+    echo "/tmp/.X11-unix exists."
+    MOUNT="$MOUNT -v /tmp/.X11-unix:/tmp/.X11-unix"
+fi
+
 docker run -it --rm \
-    -v `pwd`:`pwd` \
+    $MOUNT \
     -w `pwd` \
     -u `id -u`:`id -g` \
     -e DISPLAY=$DISPLAY \
